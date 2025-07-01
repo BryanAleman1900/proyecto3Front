fdf1632f266183f65d66f88b8613efc612c03163
alemanrouteminer <133164860+alemanrouteminer@users.noreply.github.com>
2025-06-30 20:37:12 -0600

    Angular
    New Angular

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# proyecto3Front
-Angular
+# set nodejs version
+nvm install 20.12.2
+nvm use 20.12.2
+
+# install node modules
+npm install
+
+# install node modules
+npm start
---
+++ src/app/app.routes.ts
@@ -0,0 +1,111 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { SigUpComponent } from './pages/auth/sign-up/signup.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AccessDeniedComponent } from './pages/access-denied/access-denied.component';
+import { AdminRoleGuard } from './guards/admin-role.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GuestGuard } from './guards/guest.guard';
+import { IRoleType } from './interfaces';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { ProductComponent } from './pages/product/product.component';
+
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [GuestGuard],
+  },
+  {
+    path: 'signup',
+    component: SigUpComponent,
+    canActivate: [GuestGuard],
+  },
+  {
+    path: 'access-denied',
+    component: AccessDeniedComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'app',
+    component: AppLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'app',
+        redirectTo: 'users',
+        pathMatch: 'full',
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+        canActivate:[AdminRoleGuard],
+        data: { 
+          authorities: [
+            IRoleType.admin, 
+            IRoleType.superAdmin
+          ],
+          name: 'Users',
+          showInSidebar: true
+        }
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        data: { 
+          authorities: [
+            IRoleType.admin, 
+            IRoleType.superAdmin,
+            IRoleType.user
+          ],
+          name: 'Dashboard',
+          showInSidebar: true
+        }
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        data: { 
+          authorities: [
+            IRoleType.admin, 
+            IRoleType.superAdmin,
+            IRoleType.user
+          ],
+          name: 'profile',
+          showInSidebar: false
+        }
+      },
+
+      {
+  path: 'categories',
+  component: CategoryComponent,
+  canActivate: [AdminRoleGuard],
+  data: { 
+    authorities: [IRoleType.superAdmin],
+    name: 'Categorías',
+    showInSidebar: true
+  }
+},
+{
+  path: 'products',
+  component: ProductComponent, // importa esto arriba
+  data: { 
+    authorities: [
+      IRoleType.admin, 
+      IRoleType.superAdmin
+    ],
+    name: 'Products',
+    showInSidebar: true
+  }
+},
+    ],
+  },
+];
